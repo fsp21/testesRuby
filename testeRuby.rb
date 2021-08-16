302e6ed48cb2be9d3d35ff1adce35769258fbf10
Felipe Alves Sao Paulo <felipe.paulo@pampa.compasso>
2021-08-16 15:40:42 -0300

    Testando alterações e commit

--- testeRuby.rb
+++ testeRuby.rb
@@ -1,3 +1,6 @@
 puts "Qual é o seu nome?"
 nome = gets.chomp
 puts "Seja muito bem vindo(a), " + nome + "!"
+puts "Agora, por favor, nos diga a sua idade:"
+idade = gets.chomp
+puts "Obrigado, " + nome + "! Registramos que você tem " + idade + " anos."
