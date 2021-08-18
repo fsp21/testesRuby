65185bef248493946e405905dd2ed5ee870d32a2
Felipe Alves Sao Paulo <felipe.paulo@pampa.compasso>
2021-08-18 09:51:53 -0300

    Resolução do exercício proposto aula 4

---
+++ Exercícios propostos/aula4terceiroexercicio.rb
@@ -0,0 +1,23 @@
+# Dado o seguinte hash:
+
+# numbers = {A: 10, B: 30, C: 20, D: 25, E: 15}
+
+# Crie uma instrução que seleciona o maior valor deste hash e, no final, imprima a chave e valor do elemento resultante.
+
+numbers = {A: 10, B: 30, C: 20, D: 25, E:15}
+
+t = 0
+hash = {}
+numbers.each do |x, y|
+    if y > t
+        t = y
+end
+end
+
+numbers.each do |x, y|
+    if y >= t
+       hash[x] = y
+end
+end
+
+puts hash
